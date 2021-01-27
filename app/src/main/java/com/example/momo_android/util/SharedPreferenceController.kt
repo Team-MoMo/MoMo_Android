c415d9cf42b57113100558b11f7189f1a2957768
yunso <cw2cu96@gmail.com>
2021-01-27 18:18:45 +0900

    [feat] 로그아웃 기능 구현

--- app/src/main/java/com/example/momo_android/util/SharedPreferenceController.kt
+++ app/src/main/java/com/example/momo_android/util/SharedPreferenceController.kt
@@ -5,7 +5,6 @@ import android.content.Context
 object SharedPreferenceController {
 
     private val TOKEN = "TOKEN"
-    private val MY_ACCOUNT = "MY_ACCOUNT"
     private val ON_BOARDING = "ON_BOARDING"
     private val USER_ID = "USER_ID"
 
@@ -23,6 +22,11 @@ object SharedPreferenceController {
         return pref.getString("access_token", "")
     }
 
+    fun clearAccessToken(context: Context) {
+        val pref = context.getSharedPreferences(TOKEN, Context.MODE_PRIVATE)
+        pref.edit().clear().apply()
+    }
+
     // userId 유저아이디 = 서버에 보내주는 Int값
     fun setUserId(context: Context, userId: Int) {
         val pref = context.getSharedPreferences(USER_ID, Context.MODE_PRIVATE)
@@ -36,36 +40,9 @@ object SharedPreferenceController {
         return pref.getInt("USER_ID", 0)
     }
 
-    // 자동로그인 & 메일, 비밀번호 정보
-    fun setMail(context: Context, input: String) {
-        val prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor = prefs.edit()
-        editor.putString("MAIL", input)
-        editor.apply()
-    }
-
-    fun getMail(context: Context) : String? {
-        val prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        return prefs.getString("MAIL", "")
-    }
-
-    fun setPasswd(context: Context, input: String) {
-        val prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor = prefs.edit()
-        editor.putString("PW", input)
-        editor.apply()
-    }
-
-    fun getPasswd(context: Context) : String? {
-        val prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        return prefs.getString("PW", "")
-    }
-
-    fun clearUser(context: Context) {
-        val prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor = prefs.edit()
-        editor.clear()
-        editor.apply()
+    fun clearUserId(context: Context) {
+        val pref = context.getSharedPreferences(USER_ID, Context.MODE_PRIVATE)
+        pref.edit().clear().apply()
     }
 
 
@@ -81,6 +58,4 @@ object SharedPreferenceController {
         val prefs = context.getSharedPreferences(ON_BOARDING, Context.MODE_PRIVATE)
         return prefs.getString("ON_BOARDING", "")
     }
-
-
 }
